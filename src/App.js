5f94b8341e356d2c1c669a39abca35d3dff98568
Lev N <lev-n@lev-pro.local>
2021-02-25 16:39:41 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react'
 import Home from './components/Home';
-import Nav from './components/Nav';
-import { Router } from '@reach/router';
-import About from './components/About';
-import ContactUs from './components/ContactUs';
 import './style.css';
 
-export class App extends Component {
+class App extends Component {
 	render() {
 		return (
-			<Router>
-				<Home path="/" />
-				<About path="/about" />
-				<ContactUs path="/contact-us" />
-			</Router>
+			<Home data="Some data" />
 		)
 	}
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,39 @@
-import React from 'react'
-import Nav from './Nav'
+import React, { Component } from 'react'
 
-const Home = () => (
-	<div>
-		<Nav/>
-		This is Home
-	</div>
-);
+class Home extends Component {
+
+	constructor(props) {
+		super(props)
+	
+		this.state = {
+			 name: 'ma4ine'
+		}
+	}
+	
+
+	static getDerivedStateFromProps( props, state ) {
+		console.log( 'getDerivedStateFromProps is called' );
+		console.log( 'props', props );
+		console.log( 'state', state );
+		return null;
+	}
+
+	handleClickEvent = () => {
+		// this.setState( { name: 'Ryan' } ); // set state method
+		this.forceUpdate( () => {
+			console.log('The component is update');
+			this.setState( { name: 'Irene' } )
+		} )
+	}
+
+	render() {
+		console.log( 'render is called' );
+		return (
+			<div>
+				 <button onClick={this.handleClickEvent}>Click</button>
+			</div>
+		)
+	}
+}
 
 export default Home
