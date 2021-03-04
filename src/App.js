ff6c652da1c4dac5717005e382ef8da9652b11c4
Lev N <lev-n@lev-pro.local>
2021-03-04 16:13:59 +0300

    Component Mount & Unmount

--- src/App.js
+++ src/App.js
@@ -3,10 +3,24 @@ import Home from './components/Home';
 import './style.css';
 
 class App extends Component {
+
+	constructor(props) {
+		super(props)
+	
+		this.state = {
+			 show: false
+		}
+	}
+	
+	handleOnClick = () => {
+		this.setState( { show: ! this.state.show } ) // simple toggler
+	}
+
 	render() {
 		return (
 			<div>
-				<Home name="ma4ine" />
+				{ this.state.show && <Home name="ma4ine" /> }
+				<button onClick={this.handleOnClick}>Click</button>
 			</div>
 		)
 	}
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,37 +3,54 @@ import React, { cloneElement, Component } from 'react'
 class Home extends Component {
 
 	constructor(props) {
-		super(props)
+		super(props);
+
+		console.log('constructor called');
 	
 		this.state = {
-			 age: ''
+			data: '',
+			loading: true
 		}
 	}
 
-	handleOnClick = () => {
-		this.setState( { 
-			age: 35 
-		} )
+	componentDidMount() {
+		console.log('didMount called');
+		fetch('https://jsonplaceholder.typicode.com/todos/1')
+  		.then(response => response.json())
+  		.then(json => {
+				// console.log( 'data', json );
+				this.setState( { 
+					data: json,
+					loading: false
+				} );
+			})
 	}
 
-	getSnapshotBeforeUpdate( prevProps, prevState ) {
-		console.log('getSnapshotBeforeUpdate called');
-		console.log('prevProps', prevProps);
-		console.log('prevState', prevState);
-		return 'xyz';
+	componentWillUnmount() {
+		console.log('Component Unmounted');
 	}
 
-	componentDidUpdate( prevProps, prevSate, snapshot ) {
-		console.log('componentDidUpdate called');
-		console.log('snapshot', snapshot)
+	renderData = () => {
+		const mydata = this.state.data;
+		if ( mydata.id ) {
+			return (
+				<div>
+					<h1>Title : { mydata.title }</h1>
+					<p>With the id : { mydata.id }</p>
+				</div>
+			)
+		} else {
+			return <p>{ this.state.loading && 'Loading...' }</p>
+		}
 	}
 
 	render() {
 		console.log('render is called');
-		console.log( 'rendered', this.props );
+		// console.log( 'rendered', this.props );
 		return(
 			<div>
-				<button onClick={ this.handleOnClick }>Click me!</button>
+				{/* <button onClick={ this.handleOnClick }>Click me!</button> */}
+				{ this.renderData() }
 			</div>
 		)
 	}
